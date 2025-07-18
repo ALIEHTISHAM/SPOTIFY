bfd9e20e44340cb84b2b2c193a060574a62dd81e
ALIEHTISHAM <maaliehtisham@gmail.com>
2025-07-18 19:37:17 +0500

    crazy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL;
+let API_URL = process.env.REACT_APP_API_URL;
+if (API_URL.endsWith('/')) {
+  API_URL = API_URL.slice(0, -1);
+}
 export default API_URL; 
