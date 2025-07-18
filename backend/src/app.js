f33673cc6aab54d351a20ccf42e50789ddd62911
ALIEHTISHAM <maaliehtisham@gmail.com>
2025-07-18 18:36:06 +0500

    crazy

--- backend/src/app.js
+++ backend/src/app.js
@@ -72,9 +72,4 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app; 
